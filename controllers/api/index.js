f341c73f6250ddbc161b61a34082bd54220a30d0
Liz FIschenich <Liz.Devlin85@gmail.com>
2023-10-29 20:42:44 -0500

    add views to structure--not sure how that got missed. start main handlebars file. TODO fix server connection, add css, flesh out views folder.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const articleRoutes = require('./articleRoutes');
+const articleRoutes = require('../articleRoutes');
 
 router.use('/articeRoutes', articleRoutes);
 
